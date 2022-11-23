a3aaa70c7204c69ce2a10be87ca11cc7eab2fce6
Powershell-Graphics <118919449+Powershell-Graphics@users.noreply.github.com>
2022-11-23 16:16:50 +0000

    Add files via upload

---
+++ 010 - Basic JPG with a BackGround Colour.ps1
@@ -0,0 +1,23 @@
+﻿# Setup Vars
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+$OutputFile = "C:\temp\result.jpg" 
+
+# Create Image File 
+
+$bmp = new-object System.Drawing.Bitmap 1920,1080 
+
+# Fill Image with Colour "SlateBlue"
+
+$brushBg = [System.Drawing.Brushes]::SlateBlue
+$graphics = [System.Drawing.Graphics]::FromImage($bmp) 
+$graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height) 
+
+# Save File
+
+$bmp.Save($OutputFile) 
+
+# Tidy Up
+
+$bmp.Dispose()
+$graphics.Dispose()
