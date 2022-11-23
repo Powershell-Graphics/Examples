d75bda27b70ba8d390920690e5e105630eda8f35
David Howkins <118919449+Powershell-Graphics@users.noreply.github.com>
2022-11-23 16:45:24 +0000

    Add files via upload

---
+++ 025 - Basic JPG with a BackGround Colour (HEX).ps1
@@ -0,0 +1,25 @@
+﻿# Setup Vars
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+$OutputFile = "C:\temp\result.jpg" 
+
+# Create Image File 
+
+$bmp = new-object System.Drawing.Bitmap 1920,1080 
+
+# Fill Image with HEX Colour
+
+for ($y = 0; $y -lt $bmp.Height; $y++) {
+        for ($x = 0; $x -lt $bmp.Width; $x++) {
+            $bmp.SetPixel($x, $y, [System.Drawing.ColorTranslator]::FromHtml('#670000'))
+        }
+    }
+
+# Save File
+
+$bmp.Save($OutputFile) 
+
+# Tidy Up
+
+$bmp.Dispose()
+$graphics.Dispose()
