a70f2f2d98bcc38c6824062b1cbbae102cbe8ff7
Powershell-Graphics <118919449+Powershell-Graphics@users.noreply.github.com>
2022-11-23 16:20:37 +0000

    Add files via upload

---
+++ 020 - Basic JPG with a BackGround Colour (RGB).ps1
@@ -0,0 +1,28 @@
+﻿# Setup Vars
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+$OutputFile = "C:\temp\result.jpg" 
+
+# Create Image File 
+
+$bmp = new-object System.Drawing.Bitmap 1920,1080 
+
+# Fill Image with RBG Colour
+
+for ($y = 0; $y -lt $bmp.Height; $y++) {
+        for ($x = 0; $x -lt $bmp.Width; $x++) {
+            $varR = 68 
+            $varG = 0 
+            $varB = 153
+            $bmp.SetPixel($x, $y, [System.Drawing.Color]::FromArgb($varR, $varG, $varB))
+        }
+    }
+
+# Save File
+
+$bmp.Save($OutputFile) 
+
+# Tidy Up
+
+$bmp.Dispose()
+$graphics.Dispose()
