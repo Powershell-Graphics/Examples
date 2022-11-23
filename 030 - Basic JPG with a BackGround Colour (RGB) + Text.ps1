db8e49d9e3aae26c7aebe88e5fb2736273460daf
Powershell-Graphics <118919449+Powershell-Graphics@users.noreply.github.com>
2022-11-23 16:32:00 +0000

    Add files via upload

---
+++ 030 - Basic JPG with a BackGround Colour (RGB) + Text.ps1
@@ -0,0 +1,42 @@
+﻿# Setup Vars
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+$OutputFile = "C:\temp\result.jpg" 
+
+# Create Image File 
+
+$bmp = new-object System.Drawing.Bitmap 1920,1080 
+
+# Fill Image with RBG Colour
+
+for ($y = 0; $y -lt $bmp.Height; $y++) {
+        for ($x = 0; $x -lt $bmp.Width; $x++) {
+            $varR = 68 
+            $varG = 0 
+            $varB = 153
+            $bmp.SetPixel($x, $y, [System.Drawing.Color]::FromArgb($varR, $varG, $varB))
+        }
+    }
+
+$Graphics = [system.drawing.Graphics]::FromImage($bmp)
+$Graphics.TextRenderingHint = 'AntiAliasGridFit'
+
+$Text ="Example"
+$FontName = "Arial"
+$FontSize = 105
+$TextColour = [System.Drawing.Brushes]::"White"
+$TextPosition_Left = 100
+$TextPosition_Top = 100
+
+$Font = new-object System.Drawing.Font($FontName, $FontSize)
+
+$Graphics.DrawString($Text,$Font,$TextColour,$TextPosition_Left,$TextPosition_Top)
+
+# Save File
+
+$bmp.Save($OutputFile) 
+
+# Tidy Up
+
+$bmp.Dispose()
+$graphics.Dispose()
