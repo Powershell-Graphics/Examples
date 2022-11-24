3d07f6da2cf5a23fcea00777aea17e0ed489a5c9
David Howkins <118919449+Powershell-Graphics@users.noreply.github.com>
2022-11-24 11:44:25 +0000

    Add files via upload

---
+++ 050 - Basic JPG with a BackGround Colour with overlayed png file.ps1
@@ -0,0 +1,45 @@
+﻿# Setup Vars
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+$OutputFile = "C:\temp\result.jpg"
+$PNGFile = "C:\temp\LOGO.png"
+
+# Create Image File 
+
+$bmp = new-object System.Drawing.Bitmap 1920,1080
+
+# Create Graphics Buffer
+
+$Graphics = [system.drawing.Graphics]::FromImage($bmp)
+$Graphics.TextRenderingHint = 'AntiAliasGridFit'
+
+# Fill BG with colour
+
+$brushBg = [System.Drawing.Brushes]::SlateBlue
+$graphics = [System.Drawing.Graphics]::FromImage($bmp) 
+$graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height) 
+
+# Create Graphics Buffer for PNG File
+
+$image2 = [system.drawing.image]::FromFile($PNGFile)
+$image2Graphics = [system.drawing.Graphics]::FromImage($image2)
+$image2Graphics.TextRenderingHint = 'AntiAliasGridFit'
+
+# Overlay Image inc scale function 
+
+$Scale = 0.60
+$graphics.DrawImage($image2, 200, 200,$image2.Width*$Scale ,$image2.Height*$Scale)
+
+# Save File
+
+$bmp.Save($OutputFile) 
+
+# Tidy Up
+
+$bmp.Dispose()
+$Graphics.Dispose()
+
+# Open File When Finished
+
+& $OutputFile
